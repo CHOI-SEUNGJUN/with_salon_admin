4b7d72507abc01041dee689b0fc74306eaa89f44
artiiicy <mjun09@naver.com>
2020-08-04 16:39:23 +0900

    updates

--- admin-react/src/config/connection.js
+++ admin-react/src/config/connection.js
@@ -1,5 +1,5 @@
-// const BaseURL = 'http://52.78.41.75:3333/';
-const BaseURL = 'http://localhost:3333/';
+const BaseURL = 'http://52.78.41.75:3333/';
+//const BaseURL = 'http://localhost:3333/';
 
 module.exports = {
     BaseURL,
